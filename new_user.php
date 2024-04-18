78e809c38f35967b2672e336be59036ef94e75b5
ooilchen <125842284+ooilchen@users.noreply.github.com>
2024-04-18 21:18:57 +0800

    .

---
+++ new_user.php
@@ -0,0 +1,19 @@
+<?php
+include "conn.php";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve form data
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
+        $position = $_POST['position'];
+        $password = $_POST['password'];
+        $confirmPassword = $_POST['confirmPassword'];
+
+        $sql = "INSERT INTO `register`(`first_name`, `last_name`, `gender`, `email`, `mobile`, `position`, `password`) VALUES ('$firstName','$lastName','$gender','$email','$mobile','$position','$password')";
+        
+        $result = $conn->query($sql);
+    }
+?>
