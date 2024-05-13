9e8d097865ca1271359ad2203b48f822bb6e39b5
ooilchen <125842284+ooilchen@users.noreply.github.com>
2024-05-13 18:00:02 +0800

    .

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Gotto Online Job Portal</title>
+        <title>Portfolify</title>
 
         <!-- CSS FILES -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -31,56 +31,7 @@
     
     <body id="top">
 
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="index.html">
-                    <img src="images/logo.png" class="img-fluid logo-image">
-
-                    <div class="d-flex flex-column">
-                        <strong class="logo-text">Portfolify</strong>
-                        <small class="logo-slogan">Online Job and Project collaboration Portal</small>
-                    </div>
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav align-items-center ms-lg-5">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.html">Homepage</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.html">About Portfolify</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
-
-                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item" href="job-listings.html">Job Listings</a></li>
-
-                                <li><a class="dropdown-item" href="job-details.html">Project Listings</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.html">Contact</a>
-                        </li>
-
-                        <li class="nav-item ms-lg-auto">
-                            <a class="nav-link" href="Sign_Up.html">Register</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link custom-btn btn" href="Login.html">Login</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <?php include 'navbar.php'; ?>
 
         <main>
 
@@ -454,7 +405,7 @@
                     <?php
                         include "con.php";
 
-                        $query = "SELECT `project_ID`, `project_title`, `project_desc`, `project_price`, `project_image` FROM `project` ORDER BY `project_date` DESC LIMIT 6";
+                        $query = "SELECT * FROM `project` ORDER BY `project_date` DESC LIMIT 6";
                         $result = mysqli_query($conn, $query);
 
                         if (mysqli_num_rows($result) > 0) {
