54dedd31e613a9384079e7f2f67824f0d8f36dfe
ooilchen <125842284+ooilchen@users.noreply.github.com>
2024-05-14 15:34:20 +0800

    .

--- new_user.php
+++ new_user.php
@@ -1,25 +1,47 @@
 <?php
 include "con.php";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Retrieve form data
-        $firstName = $_POST['firstName'];
-        $lastName = $_POST['lastName'];
-        $gender = $_POST['gender'];
-        $email = $_POST['email'];
-        $mobile = $_POST['mobile'];
-        $position = $_POST['position'];
-        $password = $_POST['password'];
-        $confirmPassword = $_POST['confirmPassword'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        $sql = "INSERT INTO `register`(`first_name`, `last_name`, `gender`, `email`, `mobile`, `position`, `password`) VALUES ('$firstName','$lastName','$gender','$email','$mobile','$position','$password')";
+    // Password encryption
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Retrieve the last ID from the database
+    $getLastIdSql = "SELECT `UserID` FROM `user_profile` ORDER BY `UserID` DESC LIMIT 1";
+    $lastIdResult = $conn->query($getLastIdSql);
+
+    if ($lastIdResult->num_rows > 0) {
         
-        if ($conn->query($sql) === TRUE) {
-            
-            echo "User registration successful!";
-        } else {
-            
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+        $row = $lastIdResult->fetch_assoc();
+        $last_id = $row["UserID"];
+        $numeric_part = (int)substr($last_id, 5); 
+        $new_id = "user-" . sprintf('%05d', $numeric_part + 1); 
+        
+    } else {
+        
+        $new_id = "user-00001";
     }
+
+    // Prepare SQL statement using prepared statements and parameter binding
+    $sql = "INSERT INTO user_profile (UserID, Email, Password, FirstName, Lastname, Gender) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssss", $new_id, $email, $hashed_password, $firstName, $lastName, $gender);
+
+    // Execute the prepared statement
+    if ($stmt->execute()) {
+        echo "User registration successful!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the prepared statement
+    $stmt->close();
+    $conn->close();
+}
 ?>
--- profile.php
+++ profile.php
@@ -47,7 +47,7 @@
 
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb justify-content-center">
-                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
 
                                     <li class="breadcrumb-item active" aria-current="page">Contact</li>
                                 </ol>
@@ -63,9 +63,18 @@
                 <div class="container">
                     <div class="row justify-content-center">
 
-                        <div class="col-lg-6 col-12 mb-lg-5 mb-3">
-                            <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4722.136219194832!2d10.772202738834757!3d59.917660271855105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416fa8eba7e84d%3A0xf4e943975503fa30!2sUrtehagen%20(herb%20garden)!5e1!3m2!1sen!2sth!4v1680951932259!5m2!1sen!2sth" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <!-- <div class="col-lg-6 col-12 mb-lg-5 mb-3">
+                            <img src="profile-picture.jpg" alt="Profile Picture" class="img-fluid rounded-circle mb-3">
+                            <h4>Name</h4>
+                            <p>Other details: Role, Location, etc.</p>
+                        </div> -->
+
+                        <div class="col-lg-6 col-12 mb-lg-5 mb-3 text-center">
+                            <img src="images/profilepic_default.jpg" alt="Profile Picture" class="img-fluid rounded-circle mb-3 mx-auto" style="width: 150px; height: 150px;">
+                            <h4>Name</h4> 
+                            <p>Other details: Role, Location, etc.</p>
                         </div>
+
 
                         <div class="col-lg-5 col-12 mb-3 mx-auto">
                             <div class="contact-info-wrap">
@@ -159,8 +168,6 @@
 
                         <div class="col-lg-6 col-10">
                             <h2 class="text-white mb-2">Over 10k opening jobs</h2>
-
-                
                         </div>
 
                         <div class="col-lg-4 col-12 ms-auto">
